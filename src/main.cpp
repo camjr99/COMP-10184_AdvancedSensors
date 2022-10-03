b78fd343f547a0909d0d7ffc53eafde7b717d6dd
camjr99 <000820614@mohawkcollege.ca>
2022-10-03 17:06:58 -0400

    commenting & statement of authorship

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+//I, Cameron Randall, 000820614 guanantee that this is my work and my work only.
+
 #include <Arduino.h>
 // 1-Wire sensor communication libary 
 #include <OneWire.h> 
@@ -13,16 +15,20 @@ OneWire oneWire(oneWireBus);
 // Pass our oneWire reference to Dallas Temperature sensor  
 DallasTemperature DS18B20(&oneWire); 
 
+//Device address
 DeviceAddress deviceAddress;
 
+//variables for later use, if the sensor is connected
 bool sensorConnected = false;
 bool initalConnected = false;
 
+//simple loop to print out DeviceAddress array
 void printAddress(DeviceAddress deviceAddress) {
   for (int i = 0; i < 8; i++) {
     Serial.print(deviceAddress[i], HEX);
   }
 }
+
 
 void setup() {
     // configure the USB serial monitor 
@@ -30,21 +36,22 @@ void setup() {
  
   // Start the DS18B20 sensor 
   DS18B20.begin(); 
-  //DS18B20.getAddress(deviceAddress, 0);
 
-
+  //make some space
   Serial.println("\n");
   Serial.println("Temperature Application");
 
+  //getAddress returns a boolean & passes a value into a given variable
+  //so if we have an address, print it & set our connected variables to true
   if (DS18B20.getAddress(deviceAddress, 0)) {
     Serial.print("Found DS18B20 sensor with address: ");
     printAddress(deviceAddress);
     Serial.print("\n");
     sensorConnected = true;
     initalConnected = true;
+  //otherwise it's an error
   } else {
     Serial.println("No DS18B20 temperature sensors are installed!");
-    //exit(0);
   }
 
   
@@ -54,15 +61,16 @@ void loop() {
   float fTemp;
   String output;
 
+  //initialConnected is only modified once at start time
   if (initalConnected) {
-  // ask DS18B20 for the current temperature 
+    // ask DS18B20 for the current temperature 
     DS18B20.requestTemperatures(); 
     // fetch the temperature.  We only have 1 sensor, so the index is 0. 
     fTemp = DS18B20.getTempCByIndex(0); 
-    // print the temp to the USB serial monitor 
+    // if no sensor is connected, set our other sensor bool to false
+    //otherwise set our output depending on value range
     if (fTemp == -127) {
       sensorConnected = false;
-      //Serial.println("No DS18B20 temperature sensors are installed!");
     } else if (fTemp < 10) {
       output = "Cold!";
     } else if ((fTemp >= 10) && (fTemp < 15)) {
@@ -76,11 +84,14 @@ void loop() {
     } else if (fTemp > 35) {
       output = "Too Hot!";
     }
+    //if sensor is connected, print formatted output
     if (sensorConnected) {
       Serial.printf("Current temperature is: %4.2f deg. C or %s \n", fTemp, output);
+    //if it's not, print an error message
     } else {
       Serial.println("No DS18B20 temperature sensors are installed!");
     }
+  //initalConnected - if the sensor was *never* connected at start-time
   } else {
     //do nothing
   }
